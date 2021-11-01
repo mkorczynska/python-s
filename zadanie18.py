0ec9ea81dd39d4541b095f9683b7919cad5b011d
mkorczynska <k.martyna95@gmail.com>
2021-11-01 23:45:37 +0100

    .

---
+++ zadanie18.py
@@ -0,0 +1,26 @@
+﻿t = input("Podaj tekst do zaszyfrowania: ")
+
+# t="Ala ma kota"
+t = t.lower()
+t = t.replace(" ", "")
+p = 3
+
+
+def cezar(txt, prz):
+    k = ""
+    for l in txt:
+        # print(l,chr(ord(l)+prz))
+        s = ord(l) + prz
+        if s > ord("z"):
+            s -= 26
+        elif s < ord("a"):
+            s += 26
+        o = chr(s)
+        k += o
+    return (k)
+
+
+kod = cezar(t, p)
+print("Zakodowany tekst: ", kod)
+dekod = cezar(kod, p * (-1))
+print("Odkodowany tekst: ", dekod)
