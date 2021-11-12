2955f55f025959253b0aa168d2a3ea21de9a9fad
mkorczynska <k.martyna95@gmail.com>
2021-11-12 19:36:17 +0100

    .

--- zadanie14.py
+++ zadanie14.py
@@ -6,14 +6,22 @@ strzaly=[]
 for i in range(6):
     a=random.randint(1, 50)
     losowanie.append(a)
-    b=input(f"Podaj liczbę {i+1} z 6: ")
-    b=int(b)
-    strzaly.append(b)
-
+    b=int(input(f"Podaj liczbę {i+1} z 6: "))
+    while b>50 or b<1:
+        print("Podaj liczbę z zakresu od 1 do 50.")
+        b=int(input(f"Podaj liczbę {i+1} z 6: "))
+    else:
+        strzaly.append(b)
+    
+print("Wylosowane liczby: ")
 print(*losowanie, sep=', ')
+print("Twoje liczby: ")
 print(*strzaly, sep=', ')
 
 trafione=set(losowanie)&set(strzaly)
 
-print(f"Trafiłeś {len(trafione)} z 6 liczb. Oto one: {trafione}")
+if len(trafione)>0:
+    print(f"Trafiłeś {len(trafione)} z 6 liczb. Oto one: {trafione}")
+else:
+    print("Nie trafiłeś żadnej z liczb.")
 
---
+++ zadanie16.py
@@ -0,0 +1,51 @@
+﻿def maksimum(a, b):
+    if a>b:
+        return a
+    elif b>a:
+        return b
+    else:
+        return "liczby są równe"
+
+def minimum(a, b, c):
+    if a<b and a<c:
+        return a
+    elif b<a and b<c:
+        return b
+    else:
+        return c
+
+def nieparzysta(a):
+    if a%2==0:
+        return "parzysta"
+    else:
+        return "nieparzysta"
+
+def unikatowe(list):
+    list=set(list)
+    return list
+
+def bezwzgledna(a):
+    if a>=0:
+        return a
+    else:
+        return -a
+
+def suma_wielu(*argumenty):
+    suma = 0
+    for arg in argumenty:
+        suma += arg
+    return suma
+
+
+a=0
+b=-10
+c=7
+
+list=[1,2,3,1,2,3]
+
+print(maksimum(a, b))
+print(minimum(a, b, c))
+print(nieparzysta(c))
+print(unikatowe(list))
+print(bezwzgledna(b))
+print(suma_wielu(a, b, c))
